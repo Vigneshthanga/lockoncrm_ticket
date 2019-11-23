32463d4a93b3c39ec3f04d4ef1dd454e455e09de
Vigneshthanga <vigneshkumar.thangarajan@gmail.com>
2019-11-22 20:14:14 -0800

    first commit

--- routes.py
+++ routes.py
@@ -5,7 +5,7 @@ from flask_principal import RoleNeed, UserNeed, identity_loaded, identity_change
 from marshmallow import Schema,fields
 from marshmallow import pprint  #testing purpose
 from models import db, user, ticket
-from flask_sqlalchemy import SQLAlchemy 
+from flask_sqlalchemy import SQLAlchemy
 import simplejson
 from models import load_db
 from sqlalchemy.exc import IntegrityError
@@ -15,7 +15,7 @@ import datetime
 
 
 
-app = Flask(__name__)	
+app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'pooja'  #need to be changed
 
@@ -55,7 +55,7 @@ class UserSchema(Schema):
 users_schema = UserSchema(many=True)  # List of objects
 
 
-@app.route("/query user", methods=["POST"])
+@app.route("/ticket/query user", methods=["POST"])
 def query_user():
 	all_users = user.query.all()
 
@@ -63,7 +63,7 @@ def query_user():
 	return simplejson.dumps({"data":schema_result.data})
 
 
-@app.route("/query user only team members", methods=["POST"])
+@app.route("/ticket/query user only team members", methods=["POST"])
 def query_user_only_team_members():
 	all_users = user.query.filter(user.rolename == "team member")
 
@@ -78,10 +78,10 @@ class ticketSchema(Schema):
 tickets_schema = ticketSchema(many=True)  # List of objects
 
 
-@app.route("/query ticket", methods=["POST"])
+@app.route("/ticket/query ticket", methods=["POST"])
 def query_ticket():
 
-	
+
 	with db.session.no_autoflush:
 		if(current_user.rolename == "team member"):
 			all_tickets = ticket.query.filter(ticket.assignedto_name == current_user.id).all()
@@ -113,7 +113,7 @@ def filter_user(requested):
 	data = user.query.filter_by(username=str(requested['username']),rolename=str(requested['rolename']),name=str(requested['name'])).first()
 	return data
 
-@app.route('/create user',methods=['POST'])
+@app.route('/ticket/create user',methods=['POST'])
 def create_user():
 	data = request.json
 	row = user(data[0],data[0],data[1],data[3],data[2])
@@ -124,36 +124,27 @@ def create_user():
 	except IntegrityError:
 		pprint("Error: Duplication of Primary Key")
 
-	return "created"	
+	return "created"
 
-
-
-
-
-@app.route('/update user',methods=['POST'])
+@app.route('/ticket/update user',methods=['POST'])
 def update_user():
 	requested = request.json
 	row = filter_user(requested[4][0])
-	if(row != None):		
+	if(row != None):
 			row.username = str(requested[0])
 			row.name = str(requested[1])
 			row.rolename = str(requested[3])
 			row.active = str(requested[2])
-	db.session.commit()		
+	db.session.commit()
 	return "updated"
-	
-	
 
-
-
-
-@app.route('/file issue',methods=['POST','GET'])
+@app.route('/ticket/file issue',methods=['POST','GET'])
 @login_required
 @client_permission.require(http_exception=403)
 def fileissue():
 	form = fileissueform()
 
-	if request.method == 'GET':	
+	if request.method == 'GET':
 		return render_template('client/pages/issue box.html',form=form)
 	issue = form.issue.data
 	subject = form.subject.data
@@ -166,27 +157,27 @@ def fileissue():
 	db.session.commit()
 	return redirect(url_for('fileissue'))
 
-@app.route('/view tickets')
+@app.route('/ticket/view tickets')
 @login_required
 @client_permission.require(http_exception=403)
 def viewtickets():
 	return render_template('client/pages/view ticket.html')
 
-@app.route('/manage users')
+@app.route('/ticket/manage users')
 @login_required
 @administrator_permission.require(http_exception=403)
 def manageusers():
 	return render_template('administrator/pages/manage users.html')
 
 
-@app.route('/manage tickets')
+@app.route('/ticket/manage tickets')
 @login_required
 @administrator_permission.require(http_exception=403)
 def managetickets():
 	return render_template('administrator/pages/manage tickets.html')
 
 
-@app.route('/manage tickets team member',methods=['POST','GET'])
+@app.route('/ticket/manage tickets team member',methods=['POST','GET'])
 @login_required
 @team_member_permission.require(http_exception=403)
 def manageticketsteammember():
@@ -205,7 +196,7 @@ def manageticketsteammember():
 	db.session.commit()
 	return "OK"
 
-@app.route('/manage tickets project manager',methods=['POST','GET'])
+@app.route('/ticket/manage tickets project manager',methods=['POST','GET'])
 @login_required
 @project_manager_permission.require(http_exception=403)
 def manageticketsprojectmanager():
@@ -216,13 +207,13 @@ def manageticketsprojectmanager():
 	if(row != None):
 		user_row = user.query.filter_by(username=str(requested[0])).first()
 		if (user_row.rolename == "team member"):
-			row.assignedto_name = str(user_row.id)	
+			row.assignedto_name = str(user_row.id)
 			row.status = str(requested[1])
 			row.assignedby_submittedto_name = current_user.id
 	db.session.commit()
 	return "OK"
 
-@app.route("/dashboard",methods=['GET','POST'])
+@app.route("/ticket/dashboard",methods=['GET','POST'])
 def dashboard():
 		if(current_user.rolename == 'administrator'):
 			return redirect(url_for('manageusers'))
@@ -237,8 +228,8 @@ def dashboard():
 
 app.jinja_env.globals.update(dashboard=dashboard)
 
-@app.route("/",methods=['GET','POST'])
-@app.route("/login",methods=['GET','POST'])
+#@app.route("/",methods=['GET','POST'])
+@app.route("/ticket/login",methods=['GET','POST'])
 def login():
 		form = loginform()
 
@@ -247,21 +238,21 @@ def login():
 
 		username = form.username.data
 		password = form.password.data
-	
+
 		user_accessed = user.query.filter_by(username=username).first()
-	
+
 		if user_accessed is None:
 			flash(u'Username is incorrect')  # to log incorrect username
 			return redirect(url_for('login'))
-	
+
 		if not user_accessed.verify_password(password):
 			flash(u'Password is incorrect')  # to log incorrect password
 			return redirect(url_for('login'))
-		
+
 		if not user_accessed.active:
 			flash(u'Your account is inactive')  # to log inactive user
 			return redirect(url_for('login'))
-	
+
 		login_user(user_accessed)
 
 		identity_changed.send(app, identity=Identity(user_accessed.username))
@@ -270,7 +261,7 @@ def login():
 
 
 
-@app.route("/logout")
+@app.route("/ticket/logout")
 @login_required
 def logout():
 	logout_user()

2f7ad939cc1ec7589fa8162dc589e67bc2e87c2a
Vigneshthanga <vigneshkumar.thangarajan@gmail.com>
2019-11-23 15:05:34 -0800

    fixing css errors

--- forms/login_form.py
+++ forms/login_form.py
@@ -1,4 +1,4 @@
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
 from wtforms.validators import InputRequired, Length
 
@@ -6,5 +6,3 @@ class loginform(Form):
 	username = StringField(u'User Name: ',validators=[InputRequired(),Length(max=30)],render_kw={"placeholder": "Email ID","class":"form-control","size":"15"})
 	password = PasswordField(u'Password: ',validators=[Length(min=1,max=16)],render_kw={"placeholder": "Password","type": "password","class":"form-control","size":"15"})
 
-
-		

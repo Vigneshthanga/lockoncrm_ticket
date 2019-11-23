497bbd92b4f562f074f9e7bc3bd77b30eb4f095d
Vigneshthanga <vigneshkumar.thangarajan@gmail.com>
2019-11-23 15:58:52 -0800

    requirements.txt

--- forms/login_form.py
+++ forms/login_form.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
 from wtforms.validators import InputRequired, Length
 
-class loginform(Form):
+class loginform(FlaskForm):
 	username = StringField(u'User Name: ',validators=[InputRequired(),Length(max=30)],render_kw={"placeholder": "Email ID","class":"form-control","size":"15"})
 	password = PasswordField(u'Password: ',validators=[Length(min=1,max=16)],render_kw={"placeholder": "Password","type": "password","class":"form-control","size":"15"})
 
